3e0e4a53178d2960a913dd9b690f4d0d380f6276
Eric Coetzee <36683674+EricCoetzee@users.noreply.github.com>
2021-01-23 08:09:14 -0800

    Create enum.sol

---
+++ enum.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract myContract {
+    enum State { Waiting, Ready, Active }
+    State public state;
+    
+    constructor() public {
+        state = State.Waiting;
+    }
+    
+    function activate() public{
+        state = State.Active;
+    }
+    
+    function isActive() public view returns(bool) { 
+        return state == State.Active;
+    }
+      
+}
